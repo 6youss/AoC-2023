2874d4450a8eb60c8164d8152ad9fa8f3877e10c
6youss <youcef.zahariou@gmail.com>
2023-12-04 03:57:42 +0100

    init day2

---
+++ day2/src/main.rs
@@ -0,0 +1,10 @@
+mod games_record;
+
+use games_record::GAMES_RECORD;
+fn main() {
+    const RED: u32 = 12;
+    const GREEN: u32 = 13;
+    const BLUE: u32 = 14;
+
+    println!("{}", GAMES_RECORD);
+}
