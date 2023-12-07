7d1d3e84a93a84ed35c68410be98bd15a5c57878
Youcef Zahariou <youcef.zahariou@gmail.com>
2023-12-07 02:10:49 +0100

    wip day3

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -3,5 +3,15 @@ use crate::engine_schematic::ENGINE_SCHEMATIC;
 mod engine_schematic;
 
 fn main() {
-    println!("{}", ENGINE_SCHEMATIC);
+    let lines: Vec<&str> = ENGINE_SCHEMATIC.trim().split('\n').collect();
+    let num_rows = lines.len();
+    let num_cols = lines[0].chars().count();
+
+    for i in 0..num_rows {
+        for j in 0..num_cols {
+            let character = lines[i].chars().nth(j).unwrap();
+            let is_digit = character.is_digit(10);
+            let is_symbol = !is_digit && !(character == '.');
+        }
+    }
 }
