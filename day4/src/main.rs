44bd328dada5a7062069e35f24f42b66c877efb7
Youcef Zahariou <youcef.zahariou@gmail.com>
2023-12-08 18:22:47 +0100

    ♻️

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -2,15 +2,14 @@ use puzzle_input::PUZZLE_INPUT;
 use std::time::{Duration, Instant};
 mod puzzle_input;
 fn main() {
-    // Record the start time
     let start_time = Instant::now();
 
-    // Perform the task or block of code for which you want to measure the execution time
-    // For example, a time-consuming operation or function
-    let mut sum = 0;
-    for i in 0..100_000 {
-        sum = 0;
-        // Do some computation or task here
+    for i in 0..1 {
+        if i % 1000 == 0 {
+            println!("benchmark index {}", i);
+        }
+
+        let mut sum = 0;
         for card in PUZZLE_INPUT.split('\n') {
             let table_row: Vec<&str> = card.split(":").collect::<Vec<&str>>();
 
@@ -35,21 +34,12 @@ fn main() {
             let score = u32::pow(2, number_owned_winning) / 2;
 
             sum += score;
-            if i % 1000 == 0 {
-                print!("{}", i as u32);
-            }
         }
+        println!("Final sum {}", sum);
     }
-    println!("{}", sum);
 
-    // Record the end time
     let end_time = Instant::now();
-
-    // Calculate the duration by subtracting start time from end time
     let duration: Duration = end_time - start_time;
-
-    // Convert the duration to milliseconds or other units if needed
     let duration_ms = duration.as_millis();
-
     println!("Execution took: {} milliseconds", duration_ms);
 }
