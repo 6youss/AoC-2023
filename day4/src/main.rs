a89510f799640231d0e55cec3f23f1d3292f42b1
Youcef Zahariou <youcef.zahariou@gmail.com>
2023-12-08 17:55:11 +0100

    add benchmark to day 4

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,37 +1,55 @@
 use puzzle_input::PUZZLE_INPUT;
-
+use std::time::{Duration, Instant};
 mod puzzle_input;
 fn main() {
+    // Record the start time
+    let start_time = Instant::now();
+
+    // Perform the task or block of code for which you want to measure the execution time
+    // For example, a time-consuming operation or function
     let mut sum = 0;
-    for card in PUZZLE_INPUT.split('\n') {
-        let table_row: Vec<&str> = card.split(":").collect::<Vec<&str>>();
+    for i in 0..100_000 {
+        sum = 0;
+        // Do some computation or task here
+        for card in PUZZLE_INPUT.split('\n') {
+            let table_row: Vec<&str> = card.split(":").collect::<Vec<&str>>();
 
-        let card_title = table_row[0];
+            let numbers_list = table_row[1].split("|").collect::<Vec<&str>>();
+            let winning: Vec<i32> = numbers_list[0]
+                .split_whitespace()
+                .filter_map(|s| s.parse().ok())
+                .collect();
+            let owned: Vec<i32> = numbers_list[1]
+                .split_whitespace()
+                .filter_map(|s| s.parse().ok())
+                .collect();
 
-        let numbers_list = table_row[1].split("|").collect::<Vec<&str>>();
-        let winning: Vec<i32> = numbers_list[0]
-            .split_whitespace()
-            .filter_map(|s| s.parse().ok())
-            .collect();
-        let owned: Vec<i32> = numbers_list[1]
-            .split_whitespace()
-            .filter_map(|s| s.parse().ok())
-            .collect();
+            let mut number_owned_winning = 0;
 
-        let mut number_owned_winning = 0;
+            for &element in &owned {
+                if winning.contains(&element) {
+                    number_owned_winning += 1;
+                }
+            }
 
-        for &element in &owned {
-            if winning.contains(&element) {
-                number_owned_winning += 1;
+            let score = u32::pow(2, number_owned_winning) / 2;
+
+            sum += score;
+            if i % 1000 == 0 {
+                print!("{}", i as u32);
             }
         }
-
-        let score = u32::pow(2, number_owned_winning) / 2;
-        println!(
-            "{}: number_owned_winning:{} score:{}",
-            card_title, number_owned_winning, score
-        );
-        sum += score;
     }
-    println!("sum {}", sum);
+    println!("{}", sum);
+
+    // Record the end time
+    let end_time = Instant::now();
+
+    // Calculate the duration by subtracting start time from end time
+    let duration: Duration = end_time - start_time;
+
+    // Convert the duration to milliseconds or other units if needed
+    let duration_ms = duration.as_millis();
+
+    println!("Execution took: {} milliseconds", duration_ms);
 }
