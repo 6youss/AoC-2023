57ec6ab4d11a99a3017610defc6d45a501dbd4e6
6youss <youcef.zahariou@gmail.com>
2023-12-09 03:08:42 +0100

    day4 done

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,41 +1,59 @@
 use puzzle_input::PUZZLE_INPUT;
-use std::time::{Duration, Instant};
+use std::{
+    collections::HashMap,
+    time::{Duration, Instant},
+};
 mod puzzle_input;
 fn main() {
     let start_time = Instant::now();
 
     for i in 0..1 {
         if i % 1000 == 0 {
-            println!("benchmark index {}", i);
+            println!("benchmark execution arrived at {}", i);
         }
 
         let mut sum = 0;
-        for card in PUZZLE_INPUT.split('\n') {
+
+        let mut cards_count_map: HashMap<usize, u32> = HashMap::new();
+
+        for (card_index, card) in PUZZLE_INPUT.split('\n').enumerate() {
             let table_row: Vec<&str> = card.split(":").collect::<Vec<&str>>();
-
             let numbers_list = table_row[1].split("|").collect::<Vec<&str>>();
-            let winning: Vec<i32> = numbers_list[0]
+            let winning_numbers: Vec<i32> = numbers_list[0]
                 .split_whitespace()
                 .filter_map(|s| s.parse().ok())
                 .collect();
-            let owned: Vec<i32> = numbers_list[1]
+            let owned_numbers: Vec<i32> = numbers_list[1]
                 .split_whitespace()
                 .filter_map(|s| s.parse().ok())
                 .collect();
 
-            let mut number_owned_winning = 0;
+            let mut matching_numbers_count = 0;
 
-            for &element in &owned {
-                if winning.contains(&element) {
-                    number_owned_winning += 1;
+            for &owned_number in &owned_numbers {
+                if winning_numbers.contains(&owned_number) {
+                    matching_numbers_count += 1;
                 }
             }
 
-            let score = u32::pow(2, number_owned_winning) / 2;
+            let card_count = cards_count_map.entry(card_index).or_insert(1).clone();
 
+            for i in (card_index + 1)..(card_index + matching_numbers_count + 1) {
+                let entry: &mut u32 = cards_count_map.entry(i).or_insert(1);
+                *entry += card_count;
+            }
+
+            let score = u32::pow(2, matching_numbers_count as u32) / 2;
             sum += score;
         }
-        println!("Final sum {}", sum);
+
+        for (card, card_count) in &cards_count_map {
+            println!("card: {}, card_count: {}", card, card_count);
+        }
+
+        let cards_count = cards_count_map.values().fold(0, |acc, &value| acc + value);
+        println!("part1: score {}", sum);
+        println!("part2: cards_count {}", cards_count);
     }
 
     let end_time = Instant::now();
