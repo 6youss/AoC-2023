c3ebc12f00132d02a7c14fa16ce125a6d7c81a1e
6youss <youcef.zahariou@gmail.com>
2023-12-11 21:38:58 +0100

    refactor day3

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -5,19 +5,7 @@ use std::ops::Range;
 mod puzzle_input;
 
 #[derive(Debug, PartialEq, Eq, Hash)]
-enum Category {
-    Seed,
-    Soil,
-    Fertilizer,
-    Water,
-    Light,
-    Temperature,
-    Humidity,
-    Location,
-}
-#[derive(Debug, PartialEq, Eq, Hash)]
-struct Node {
-    category: Category,
+struct CategoryMapper {
     source: Range<u64>,
     dest: Range<u64>,
 }
@@ -25,15 +13,26 @@ struct Node {
 fn main() {
     let almanac_lines = ALMANAC.split("\n").filter(|&line| !line.trim().is_empty());
 
-    let mut seeds: Vec<u64> = Vec::new();
+    let mut seeds_ranges: Vec<Range<u64>> = Vec::new();
     let mut currently_mapping = "";
-    let mut graph: Vec<Node> = Vec::new();
+
+    let mut seed_mappers: Vec<CategoryMapper> = Vec::new();
+    let mut soil_mappers: Vec<CategoryMapper> = Vec::new();
+    let mut fertilizer_mappers: Vec<CategoryMapper> = Vec::new();
+    let mut water_mappers: Vec<CategoryMapper> = Vec::new();
+    let mut light_mappers: Vec<CategoryMapper> = Vec::new();
+    let mut temperature_mappers: Vec<CategoryMapper> = Vec::new();
+    let mut humidity_mappers: Vec<CategoryMapper> = Vec::new();
+
     for line in almanac_lines {
         if line.contains("seeds:") {
-            seeds = line
+            seeds_ranges = line
                 .replace("seeds:", "")
                 .split_whitespace()
-                .map(|n| n.parse::<u64>().unwrap())
+                .map(|num_str| num_str.parse::<u64>().unwrap())
+                .collect::<Vec<u64>>()
+                .chunks(2)
+                .map(|chunk| chunk[0]..chunk[0] + chunk[1])
                 .collect();
         }
         let is_map_title_line = !line.chars().nth(0).unwrap().is_digit(10);
@@ -44,109 +43,299 @@ fn main() {
             let dest = map_values[0];
             let source = map_values[1];
             let range_length = map_values[2];
-            println!("{} {}", source, range_length);
             let source_range = source..source + range_length;
             let dest_range = dest..dest + range_length;
             if currently_mapping.contains("seed-to-soil") {
-                let node: Node = Node {
-                    category: Category::Seed,
+                seed_mappers.push(CategoryMapper {
                     source: source_range,
                     dest: dest_range,
-                };
-                graph.push(node);
+                });
             } else if currently_mapping.contains("soil-to-fertilizer") {
-                let node: Node = Node {
-                    category: Category::Soil,
+                soil_mappers.push(CategoryMapper {
                     source: source_range,
                     dest: dest_range,
-                };
-                graph.push(node);
+                });
             } else if currently_mapping.contains("fertilizer-to-water") {
-                let node: Node = Node {
-                    category: Category::Fertilizer,
+                fertilizer_mappers.push(CategoryMapper {
                     source: source_range,
                     dest: dest_range,
-                };
-                graph.push(node);
+                });
             } else if currently_mapping.contains("water-to-light") {
-                let node: Node = Node {
-                    category: Category::Water,
+                water_mappers.push(CategoryMapper {
                     source: source_range,
                     dest: dest_range,
-                };
-                graph.push(node);
+                });
             } else if currently_mapping.contains("light-to-temperature") {
-                let node: Node = Node {
-                    category: Category::Light,
+                light_mappers.push(CategoryMapper {
                     source: source_range,
                     dest: dest_range,
-                };
-                graph.push(node);
+                });
             } else if currently_mapping.contains("temperature-to-humidity") {
-                let node: Node = Node {
-                    category: Category::Temperature,
+                temperature_mappers.push(CategoryMapper {
                     source: source_range,
                     dest: dest_range,
-                };
-                graph.push(node);
+                });
             } else if currently_mapping.contains("humidity-to-location") {
-                let node: Node = Node {
-                    category: Category::Humidity,
+                humidity_mappers.push(CategoryMapper {
                     source: source_range,
                     dest: dest_range,
-                };
-                graph.push(node);
+                });
             }
         }
     }
-
-    let lowest = seeds
-        .iter()
-        .map(|seed| get_closest_location(&graph, &Category::Seed, *seed))
-        .min()
-        .unwrap();
-
-    println!("Lowest {}", lowest);
+    println!("{:?}", seeds_ranges);
 }
 
-fn get_closest_location(graph: &Vec<Node>, category: &Category, source_value: u64) -> u64 {
-    let source_node = graph
-        .iter()
-        .find(|n| n.category == *category && n.source.contains(&source_value));
-
-    let mut dest_value = source_value;
-    if let Some(node) = source_node {
-        dest_value = source_to_dest(&source_value, node);
-    }
-
-    let dest_category = match category {
-        Category::Seed => Category::Soil,
-        Category::Soil => Category::Fertilizer,
-        Category::Fertilizer => Category::Water,
-        Category::Water => Category::Light,
-        Category::Light => Category::Temperature,
-        Category::Temperature => Category::Humidity,
-        Category::Humidity => Category::Location,
-        Category::Location => Category::Location,
-    };
-    println!(
-        "==> {:?} {} -> {:?} {}",
-        category, source_value, dest_category, dest_value
-    );
-
-    if *category == Category::Location {
-        return dest_value;
-    }
-    return get_closest_location(graph, &dest_category, dest_value);
+#[derive(Debug, PartialEq)]
+enum RangeRelation {
+    FullyInside,
+    OutsideOnRight,
+    OutsideOnLeft,
+    PartiallyIntersectsOnLeft,
+    PartiallyIntersectsOnRight,
+    Superset,
 }
 
-fn source_to_dest(source_value: &u64, node: &Node) -> u64 {
-    let dest = if node.source.contains(&source_value) {
-        let diff = source_value - node.source.start;
-        node.dest.start + diff
+fn determine_range_relation(reference: &Range<u64>, range: &Range<u64>) -> RangeRelation {
+    if reference.start <= range.start && reference.end >= range.end {
+        RangeRelation::FullyInside
+    } else if range.start >= reference.end {
+        RangeRelation::OutsideOnRight
+    } else if reference.start >= range.end {
+        RangeRelation::OutsideOnLeft
+    } else if range.start <= reference.start && reference.end >= range.end {
+        RangeRelation::PartiallyIntersectsOnLeft
+    } else if reference.start <= range.start && reference.end <= range.end {
+        RangeRelation::PartiallyIntersectsOnRight
+    } else if reference.start > range.start && reference.end < range.end {
+        RangeRelation::Superset
     } else {
-        source_value.clone()
-    };
+        RangeRelation::FullyInside
+    }
+}
 
-    return dest;
+#[derive(PartialEq, Eq, Debug)]
+struct RangeMapingResult {
+    range: Range<u64>,
+    matched_mapper: bool,
+}
+
+fn map_range(source_range: &Range<u64>, mapper: &CategoryMapper) -> Vec<RangeMapingResult> {
+    let mut result: Vec<RangeMapingResult> = Vec::new();
+
+    let range_relation = determine_range_relation(&mapper.source, source_range);
+
+    println!("range_relation: {:?}", range_relation);
+
+    match range_relation {
+        RangeRelation::FullyInside => {
+            result.push(RangeMapingResult {
+                range: map_value_inside_range(source_range.start, &mapper)
+                    ..map_value_inside_range(source_range.end, &mapper),
+                matched_mapper: true,
+            });
+        }
+        RangeRelation::PartiallyIntersectsOnRight => {
+            result.push(RangeMapingResult {
+                range: map_value_inside_range(source_range.start, &mapper)
+                    ..map_value_inside_range(mapper.source.end, &mapper),
+                matched_mapper: true,
+            });
+            result.push(RangeMapingResult {
+                matched_mapper: false,
+                range: mapper.source.end..source_range.end,
+            });
+        }
+        RangeRelation::PartiallyIntersectsOnLeft => {
+            result.push(RangeMapingResult {
+                range: source_range.start..mapper.source.start,
+                matched_mapper: false,
+            });
+            result.push(RangeMapingResult {
+                range: map_value_inside_range(mapper.source.start, &mapper)
+                    ..map_value_inside_range(source_range.end, &mapper),
+                matched_mapper: true,
+            });
+        }
+        RangeRelation::Superset => {
+            result.push(RangeMapingResult {
+                range: source_range.start..mapper.source.start,
+                matched_mapper: false,
+            });
+            result.push(RangeMapingResult {
+                range: map_value_inside_range(mapper.source.start, &mapper)
+                    ..map_value_inside_range(mapper.source.end, &mapper),
+                matched_mapper: true,
+            });
+            result.push(RangeMapingResult {
+                range: mapper.source.end..source_range.end,
+                matched_mapper: false,
+            });
+        }
+        RangeRelation::OutsideOnLeft | RangeRelation::OutsideOnRight => {
+            result.push(RangeMapingResult {
+                range: source_range.clone(),
+                matched_mapper: false,
+            });
+        }
+    }
+
+    return result;
+}
+
+fn map_value_inside_range(value: u64, mapper: &CategoryMapper) -> u64 {
+    let diff: u64 = value - mapper.source.start;
+    mapper.dest.start + diff
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_map_range() {
+        assert_eq!(
+            map_range(
+                &(1..2),
+                &CategoryMapper {
+                    source: 0..5,
+                    dest: 50..55,
+                }
+            ),
+            vec![RangeMapingResult {
+                range: 51..52,
+                matched_mapper: true
+            }]
+        );
+        assert_eq!(
+            map_range(
+                &(5..10),
+                &CategoryMapper {
+                    source: 0..5,
+                    dest: 50..55,
+                }
+            ),
+            vec![RangeMapingResult {
+                range: 5..10,
+                matched_mapper: false
+            }]
+        );
+        assert_eq!(
+            map_range(
+                &(0..6),
+                &CategoryMapper {
+                    source: 1..5,
+                    dest: 51..55,
+                }
+            ),
+            vec![
+                RangeMapingResult {
+                    range: 0..1,
+                    matched_mapper: false
+                },
+                RangeMapingResult {
+                    range: 51..55,
+                    matched_mapper: true
+                },
+                RangeMapingResult {
+                    range: 5..6,
+                    matched_mapper: false
+                }
+            ]
+        );
+        assert_eq!(
+            map_range(
+                &(2..6),
+                &CategoryMapper {
+                    source: 1..5,
+                    dest: 51..55,
+                }
+            ),
+            vec![
+                RangeMapingResult {
+                    range: 52..55,
+                    matched_mapper: true
+                },
+                RangeMapingResult {
+                    range: 5..6,
+                    matched_mapper: false
+                },
+            ]
+        );
+        assert_eq!(
+            map_range(
+                &(0..4),
+                &CategoryMapper {
+                    source: 1..5,
+                    dest: 51..55,
+                }
+            ),
+            vec![
+                RangeMapingResult {
+                    range: 0..1,
+                    matched_mapper: false
+                },
+                RangeMapingResult {
+                    range: 51..54,
+                    matched_mapper: true
+                },
+            ]
+        );
+    }
+
+    #[test]
+    fn test_map_value_inside_range() {
+        assert_eq!(
+            map_value_inside_range(
+                5,
+                &CategoryMapper {
+                    source: 0..5,
+                    dest: 50..55,
+                }
+            ),
+            55
+        );
+        assert_eq!(
+            map_value_inside_range(
+                3,
+                &CategoryMapper {
+                    source: 0..5,
+                    dest: 50..55,
+                }
+            ),
+            53
+        );
+    }
+
+    #[test]
+    fn test_determine_range_relation() {
+        assert_eq!(
+            determine_range_relation(&(1..5), &(2..4)),
+            RangeRelation::FullyInside
+        );
+        assert_eq!(
+            determine_range_relation(&(1..2), &(1..2)),
+            RangeRelation::FullyInside
+        );
+        assert_eq!(
+            determine_range_relation(&(1..2), &(1..3)),
+            RangeRelation::PartiallyIntersectsOnRight
+        );
+        assert_eq!(
+            determine_range_relation(&(1..2), &(0..2)),
+            RangeRelation::PartiallyIntersectsOnLeft
+        );
+        assert_eq!(
+            determine_range_relation(&(1..2), &(0..3)),
+            RangeRelation::Superset
+        );
+        assert_eq!(
+            determine_range_relation(&(1..2), &(2..3)),
+            RangeRelation::OutsideOnRight
+        );
+        assert_eq!(
+            determine_range_relation(&(1..2), &(0..1)),
+            RangeRelation::OutsideOnLeft
+        );
+    }
 }
