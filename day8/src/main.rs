32c1908d0229b4b0cea1915ed26d4943632e04a7
6youss <youcef.zahariou@gmail.com>
2023-12-14 18:08:55 +0100

    day8 ⭐️⭐️ wip

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -27,6 +27,7 @@ fn main() {
 
     let mut maze: HashMap<&str, (&str, &str)> = HashMap::new();
 
+    let mut starting_nodes: Vec<&str> = Vec::new();
     for line in lines.skip(1) {
         let parts: Vec<&str> = line.split(" = ").collect();
 
@@ -36,31 +37,55 @@ fn main() {
             .split(", ")
             .collect();
         maze.insert(node, (neighbours[0], neighbours[1]));
+        if node.ends_with("A") {
+            starting_nodes.push(node);
+        }
     }
-    println!("Walked {} steps", walk_maze(&maze, &instructions, "AAA", 0));
+    println!(
+        "Walked {} steps",
+        walk_maze(&maze, &instructions, &starting_nodes, 0)
+    );
 }
 
 fn walk_maze(
     maze: &HashMap<&str, (&str, &str)>,
     instructions: &Vec<Direction>,
-    current_node: &str,
+    current_nodes: &Vec<&str>,
     step: usize,
 ) -> usize {
-    println!("Current node: {}", current_node);
-    if current_node == "ZZZ" {
+    println!("Current node: {:?}", current_nodes);
+    if all_end_with_z(current_nodes) {
         return step;
     }
     let next_direction_index = step % instructions.len();
     let next_direction = instructions[next_direction_index];
-    let navigation_instruction = maze.get(current_node).unwrap();
-    let next_node = get_next_node(*navigation_instruction, next_direction);
 
-    return walk_maze(maze, instructions, next_node, step + 1);
+    let navigation_instructions: Vec<(&str, &str)> = current_nodes
+        .iter()
+        .map(|node| *maze.get(node).unwrap())
+        .collect();
+    let next_nodes = get_next_nodes(&navigation_instructions, next_direction);
+
+    return walk_maze(maze, instructions, &next_nodes, step + 1);
 }
 
-fn get_next_node<'a>(current_node: (&'a str, &'a str), current_direction: Direction) -> &str {
+fn all_end_with_z(navigation_choices: &Vec<&str>) -> bool {
+    navigation_choices.iter().all(|node| node.ends_with("Z"))
+}
+
+fn get_next_node<'a>(navigation_choice: (&'a str, &'a str), current_direction: Direction) -> &str {
     match current_direction {
-        Direction::L => current_node.0,
-        Direction::R => current_node.1,
+        Direction::L => navigation_choice.0,
+        Direction::R => navigation_choice.1,
     }
+}
+
+fn get_next_nodes<'a>(
+    navigation_choices: &'a Vec<(&'a str, &'a str)>,
+    current_direction: Direction,
+) -> Vec<&'a str> {
+    navigation_choices
+        .iter()
+        .map(|node| get_next_node(*node, current_direction))
+        .collect()
 }
