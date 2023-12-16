0c68f1d5336f8d17dcbe499ccd4b9afd7dab0eb3
6youss <youcef.zahariou@gmail.com>
2023-12-16 20:06:39 +0100

    day9 ⭐️⭐️

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -7,9 +7,9 @@ fn main() {
         .lines()
         .map(|l| l.split_whitespace().map(|n| n.parse::<i32>().unwrap()))
     {
-        sum += get_next_value(&line.collect());
+        sum += get_previous_value(&line.collect());
     }
-    println!("⭐️ {}", sum);
+    println!("⭐️⭐️ {}", sum);
 }
 
 fn get_next_value(array: &Vec<i32>) -> i32 {
@@ -26,6 +26,30 @@ fn get_next_value(array: &Vec<i32>) -> i32 {
 
         if steps_array.iter().all(|e| *e == 0) {
             return lost_stack.iter().sum();
+        }
+    }
+}
+
+fn get_previous_value(array: &Vec<i32>) -> i32 {
+    let mut steps_array: Vec<i32> = array.iter().cloned().collect();
+    let mut lost_stack: Vec<i32> = Vec::new();
+    lost_stack.push(steps_array[0]);
+    println!("{:?}", steps_array);
+    loop {
+        for i in 1..steps_array.len() {
+            steps_array[i - 1] = steps_array[i] - steps_array[i - 1];
+        }
+        lost_stack.push(steps_array[0]);
+        steps_array.pop();
+        println!("{:?}", steps_array);
+        if steps_array.iter().all(|e| *e == 0) {
+            let mut previous_value: i32 = 0;
+            while !lost_stack.is_empty() {
+                println!("stack {:?}", lost_stack);
+                previous_value = lost_stack.pop().unwrap() - (previous_value);
+                println!("previous_value {}", previous_value);
+            }
+            return previous_value;
         }
     }
 }
