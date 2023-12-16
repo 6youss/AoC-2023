9d23d2dfab9081b37a9886f7f46158c4852f6043
6youss <youcef.zahariou@gmail.com>
2023-12-16 12:44:59 +0100

    day9 ⭐️

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -2,22 +2,30 @@ use crate::puzzle_input::PUZZLE_INPUT;
 
 mod puzzle_input;
 fn main() {
+    let mut sum = 0;
     for line in PUZZLE_INPUT
         .lines()
-        .map(|l| l.split_whitespace().map(|n| n.parse::<u32>().unwrap()))
+        .map(|l| l.split_whitespace().map(|n| n.parse::<i32>().unwrap()))
     {
-        let mut steps_array = line.clone().collect::<Vec<u32>>();
-        println!("{:?}", steps_array);
-        loop {
-            for i in 1..steps_array.len() {
-                steps_array[i - 1] = steps_array[i] - steps_array[i - 1];
-            }
-            steps_array.pop();
-            println!("{:?}", steps_array);
+        sum += get_next_value(&line.collect());
+    }
+    println!("⭐️ {}", sum);
+}
 
-            if steps_array.iter().all(|e| *e == 0) {
-                break;
-            }
+fn get_next_value(array: &Vec<i32>) -> i32 {
+    let mut steps_array: Vec<i32> = array.iter().cloned().collect();
+    let mut lost_stack: Vec<i32> = Vec::new();
+    println!("{:?}", steps_array);
+    loop {
+        for i in 1..steps_array.len() {
+            steps_array[i - 1] = steps_array[i] - steps_array[i - 1];
+        }
+        let lost_element = steps_array.pop().unwrap();
+        lost_stack.push(lost_element);
+        println!("{:?}", steps_array);
+
+        if steps_array.iter().all(|e| *e == 0) {
+            return lost_stack.iter().sum();
         }
     }
 }
