82bbd12026f77b4b48fd64fc095f3eb09b3aa406
6youss <youcef.zahariou@gmail.com>
2023-12-17 16:25:00 +0100

    day9 ⭐️ wip

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,6 +1,18 @@
+use std::collections::HashMap;
+
 use puzzle_input::PUZZLE_INPUT;
 
 mod puzzle_input;
 fn main() {
-    print!("{}", PUZZLE_INPUT);
+    let mut graph: HashMap<&char, Vec<&char>> = HashMap::new();
+    let rows: Vec<&str> = PUZZLE_INPUT.lines().collect();
+    let num_rows = rows.len();
+    let num_cols: usize = rows[0].chars().count();
+
+    // fill graph
+    for i in 0..num_rows {
+        for j in 0..num_cols {
+            let character = rows[i].chars().nth(j).unwrap();
+        }
+    }
 }
